3a72d58127df346b15515b6b2c1d232af6f5cc67
Mehrdad Kamelzadeh <mehrdad.kamelzadeh@gmail.com>
2019-03-11 23:41:58 -0400

    First chart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 }
+];
+class App extends React.Component {
+
+    render() {
+        return (
+            <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
+                <VictoryAxis
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={[1, 2, 3, 4]}
+                    tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    // tickFormat specifies how ticks should be displayed
+                    tickFormat={(x) => (`$${x / 1000}k`)}
+                />
+                <VictoryBar
+                    data={data}
+                    x="quarter"
+                    y="earnings"
+                />
+            </VictoryChart>
+        )
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
